2361b3e479d9ad5f44f6cf682af67abd80d62915
Ilya Redkin <ilyaredkin@CA-GLPH-MAC001.hitronhub.home>
2023-01-19 15:05:53 -0500

    networking done

--- networking.tf
+++ networking.tf
@@ -82,3 +82,116 @@ resource "aws_subnet" "data_subnet_b" {
   )
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.wordpress-workshop.id
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "WP Internet Gateway"
+    }
+  )
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.wordpress-workshop.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "WP Public RT"
+    }
+  )
+}
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_subnet_a.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_subnet_b.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_eip" "nat_gw_eip-a" {
+  vpc = true
+}
+
+resource "aws_eip" "nat_gw_eip-b" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat-gw-a" {
+  allocation_id = aws_eip.nat_gw_eip-a.id
+  subnet_id = aws_subnet.public_subnet_a.id
+  depends_on = [
+    aws_internet_gateway.igw
+  ]
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "NAT Public Subnet A"
+    }
+  )
+}
+
+resource "aws_nat_gateway" "nat-gw-b" {
+  allocation_id = aws_eip.nat_gw_eip-b.id
+  subnet_id = aws_subnet.public_subnet_b.id
+  depends_on = [
+    aws_internet_gateway.igw
+  ]
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "NAT Public Subnet B"
+    }
+  )
+}
+
+resource "aws_route_table" "nat_rt_a" {
+  vpc_id = aws_vpc.wordpress-workshop.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat-gw-a.id
+  }
+
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "WP NAT RT A"
+    }
+  )
+}
+
+resource "aws_route_table_association" "nat_a" {
+  subnet_id      = aws_subnet.application_subnet_a.id
+  route_table_id = aws_route_table.nat_rt_a.id
+}
+
+resource "aws_route_table" "nat_rt_b" {
+  vpc_id = aws_vpc.wordpress-workshop.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat-gw-b.id
+  }
+
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "WP NAT RT B"
+    }
+  )
+}
+
+resource "aws_route_table_association" "nat_b" {
+  subnet_id      = aws_subnet.application_subnet_b.id
+  route_table_id = aws_route_table.nat_rt_b.id
+}
